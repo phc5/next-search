74e11039f5f85c1a9b3d77fcbe9eab5ea8ab066b
phc5 <paulhyunchong@gmail.com>
2017-01-13 10:41:08 -0800

    change to youtube

--- client/js/components/form.js
+++ client/js/components/form.js
@@ -17,7 +17,7 @@ class Form extends React.Component {
         this.props.dispatch(actions.onSubmit({
             query: event.target.search.value
         }));
-        document.location = 'https://www.google.com/#q=' + this.props.query[0].query;
+        document.location = 'https://www.youtube.com/results?search_query=' + this.props.query[0].query;
     }
 
     render() {
